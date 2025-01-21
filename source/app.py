c2663e3c23261c7cdd17018c176abe5675030896
jurajvitalis <62528806+jurajvitalis@users.noreply.github.com>
2025-01-21 15:06:08 +0100

    Add avg, min devig summary

--- source/app.py
+++ source/app.py
@@ -1,5 +1,7 @@
 import os
-from flask import Flask, request, render_template
+import statistics
+
+from flask import Flask, request, render_template, make_response
 import numpy as np
 import pybettor
 import shin
@@ -12,15 +14,39 @@ app = Flask(__name__, template_folder='../templates')
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    if request.method == "POST":
-        try:
-            # Get form values
-            kelly_budget = float(request.form.get("kelly_budget"))
-            kelly_mult = float(request.form.get("kelly_mult"))
-            odds_input = request.form.get("odds_input")
-            final_odds = float(request.form.get("final_odds"))
+    # Initialize default values for result variables
+    # Retrieve Kelly values from cookies if they exist
+    kelly_budget_input = request.cookies.get("kelly_budget", "")
+    kelly_mult_input = request.cookies.get("kelly_mult", "")
+    odds_input = ""
+    final_odds = ""
+    error = None
 
-            # Parse input
+    multiplicative_results_str = None
+    additive_results_str = None
+    power_results_str = None
+    shin_results_str = None
+    min_results_str = None
+    avg_results_str = None
+    error = None
+
+    if request.method == "POST":
+        # Get inputs
+        kelly_budget_input = request.form.get("kelly_budget", "")
+        kelly_mult_input = request.form.get("kelly_mult", "")
+        odds_input = request.form.get("odds_input", "")
+        final_odds_input = request.form.get("final_odds", "")
+
+        # Validate input and perform calculations
+        try:
+            # Validate input
+            if not kelly_budget_input or not kelly_mult_input or not odds_input or not final_odds_input:
+                raise ValueError("All fields are required.")
+            kelly_budget = float(kelly_budget_input)
+            kelly_mult = float(kelly_mult_input)
+            final_odds = float(final_odds_input)
+
+            # Parse odds input
             legs = odds_input.split(',')
             legs_odds = []
             legs_probs = []
@@ -30,34 +56,66 @@ def index():
                 legs_odds.append(odds)
                 legs_probs.append(probs)
 
+            # Calculate raw margin for each leg
             margins = [calculate_margin(odds) for odds in legs_odds]
 
             # Calculations for each method
-            multiplicative_results = calculate_multiplicative_method(legs_odds, legs_probs, margins, final_odds, kelly_budget, kelly_mult)
-            additive_results = calculate_additive_method(legs_odds, legs_probs, margins, final_odds, kelly_budget, kelly_mult)
-            power_results = calculate_power_method(legs_odds, legs_probs, margins, final_odds, kelly_budget, kelly_mult)
-            shin_results = calculate_shin_method(legs_odds, legs_probs, margins, final_odds, kelly_budget, kelly_mult)
+            results = {
+                "mult": calculate_multiplicative_method(legs_odds, legs_probs, margins, final_odds, kelly_budget, kelly_mult),
+                "add": calculate_additive_method(legs_odds, legs_probs, margins, final_odds, kelly_budget, kelly_mult),
+                "power": calculate_power_method(legs_odds, legs_probs, margins, final_odds, kelly_budget, kelly_mult),
+                "shin": calculate_shin_method(legs_odds, legs_probs, margins, final_odds, kelly_budget, kelly_mult)
+            }
 
-            # Pass results to the template
-            return render_template(
-                "index.html",
-                multiplicative_results=multiplicative_results,
-                additive_results=additive_results,
-                power_results=power_results,
-                shin_results=shin_results,
-                kelly_budget=kelly_budget,
-                kelly_mult=kelly_mult,
-                odds_input=odds_input,
-                final_odds=final_odds
-            )
+            evs = [result[1][1] for result in results.items()]
+            kellys = [result[1][2] for result in results.items()]
 
-        except ValueError:
-            return render_template("index.html", error="Invalid input. Please enter valid numbers.")
+            ev_avg = round(statistics.mean(evs), 2)
+            kelly_avg = round(statistics.mean(kellys), 2)
+            avg_results_str = f"AVG: EV% = {ev_avg}%, Kelly Wager = ${kelly_avg}"
 
-    return render_template("index.html", result=None)
+            ev_min = min(evs)
+            kelly_min = min(kellys)
+            min_results_str = f"MIN: EV% = {ev_min}%, Kelly Wager = ${kelly_min}"
+
+            multiplicative_results_str = results["mult"][0]
+            additive_results_str = results["add"][0]
+            power_results_str = results["power"][0]
+            shin_results_str = results["shin"][0]
+
+        except Exception as e:
+            error = f"Invalid input. Please enter valid numbers.\n\nError: '{str(e)}'"
+
+        response = make_response(render_template(
+            "index.html",
+            multiplicative_results=multiplicative_results_str,
+            additive_results=additive_results_str,
+            power_results=power_results_str,
+            shin_results=shin_results_str,
+            min_results=min_results_str,
+            avg_results=avg_results_str,
+            kelly_budget=kelly_budget_input,
+            kelly_mult=kelly_mult_input,
+            odds_input=odds_input,
+            final_odds=final_odds_input,
+            error=error
+        ))
+
+        # Save Kelly values to cookies, with 1 year expiration
+        response.set_cookie("kelly_budget", kelly_budget_input, max_age=60 * 60 * 24 * 365)
+        response.set_cookie("kelly_mult", kelly_mult_input, max_age=60 * 60 * 24 * 365)
+
+        # Render page with calculated values
+        return response
+
+    # Request is not POST: render page, fill form with values from cookies
+    return render_template("index.html",
+                           kelly_budget=kelly_budget_input,
+                           kelly_mult=kelly_mult_input)
 
 
 def calculate_power_method(legs_odds, legs_probs, margins, final_odds, kelly_budget, kelly_mult):
+    # Devig each leg
     legs_odds_devigged = []
     legs_odds_devigged_us = []
     for i in range(len(legs_odds)):
@@ -90,10 +148,15 @@ def calculate_power_method(legs_odds, legs_probs, margins, final_odds, kelly_bud
     summary += f"<br>Final Odds ({final_odds}): Total Fair Value = {round(total_odds_devigged, 2)} " \
                f"(US {total_odds_devigged_us[0]}) ({round(1 / total_odds_devigged * 100, 2)}%)"
 
-    return f"{summary}<br>EV% = {round(total_ev, 2)}%, Kelly Wager = ${total_kelly_bet}"
+    return (
+        f"{summary}<br>EV% = {total_ev}%, Kelly Wager = ${total_kelly_bet}",
+        total_ev,
+        total_kelly_bet
+    )
 
 
 def calculate_additive_method(legs_odds, legs_probs, margins, final_odds, kelly_budget, kelly_mult):
+    # Devig each leg
     legs_odds_devigged = []
     legs_odds_devigged_us = []
     for i in range(len(legs_odds)):
@@ -126,10 +189,15 @@ def calculate_additive_method(legs_odds, legs_probs, margins, final_odds, kelly_
     summary += f"<br>Final Odds ({final_odds}): Total Fair Value = {round(total_odds_devigged, 2)} " \
                f"(US {total_odds_devigged_us[0]}) ({round(1 / total_odds_devigged * 100, 2)}%)"
 
-    return f"{summary}<br>EV% = {round(total_ev, 2)}%, Kelly Wager = ${total_kelly_bet}"
+    return (
+        f"{summary}<br>EV% = {total_ev}%, Kelly Wager = ${total_kelly_bet}",
+        total_ev,
+        total_kelly_bet
+    )
 
 
 def calculate_multiplicative_method(legs_odds, legs_probs, margins, final_odds, kelly_budget, kelly_mult):
+    # Devig each leg
     legs_odds_devigged = []
     legs_odds_devigged_us = []
     for i in range(len(legs_odds)):
@@ -162,10 +230,15 @@ def calculate_multiplicative_method(legs_odds, legs_probs, margins, final_odds, 
     summary += f"<br>Final Odds ({final_odds}): Total Fair Value = {round(total_odds_devigged, 2)} " \
                f"(US {total_odds_devigged_us[0]}) ({round(1 / total_odds_devigged * 100, 2)}%)"
 
-    return f"{summary}<br>EV% = {round(total_ev, 2)}%, Kelly Wager = ${total_kelly_bet}"
+    return (
+        f"{summary}<br>EV% = {total_ev}%, Kelly Wager = ${total_kelly_bet}",
+        total_ev,
+        total_kelly_bet
+    )
 
 
 def calculate_shin_method(legs_odds, legs_probs, margins, final_odds, kelly_budget, kelly_mult):
+    # Devig each leg
     legs_odds_devigged = []
     legs_odds_devigged_us = []
     for i in range(len(legs_odds)):
@@ -198,13 +271,17 @@ def calculate_shin_method(legs_odds, legs_probs, margins, final_odds, kelly_budg
     summary += f"<br>Final Odds ({final_odds}): Total Fair Value = {round(total_odds_devigged, 2)} " \
                f"(US {total_odds_devigged_us[0]}) ({round(1 / total_odds_devigged * 100, 2)}%)"
 
-    return f"{summary}<br>EV% = {round(total_ev, 2)}%, Kelly Wager = ${total_kelly_bet}"
+    return (
+        f"{summary}<br>EV% = {total_ev}%, Kelly Wager = ${total_kelly_bet}",
+        total_ev,
+        total_kelly_bet
+    )
 
 
 if __name__ == "__main__":
-    # # Run in localhost
-    # app.run(debug=True)
+    # Run in localhost
+    app.run(debug=True)
 
-    # Run in hosting
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    # # Run in hosting
+    # port = int(os.environ.get("PORT", 5000))
+    # app.run(host="0.0.0.0", port=port)
